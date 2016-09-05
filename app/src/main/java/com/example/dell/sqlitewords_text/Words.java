b71f8c5640ab207a03ccb463422130d6b67d587d
Violet <xinbai16@student.hh.se>
2016-09-05 22:27:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dell/sqlitewords_text/Words.java
@@ -0,0 +1,15 @@
+package com.example.dell.sqlitewords_text;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by DELL on 2016/9/5.
+ */
+public class Words {
+    public static abstract class Word implements BaseColumns {
+        public static final String TABLE_NAME="words";
+        public static final String COLUMN_NAME_WORD="word";//列：单词
+        public static final String COLUMN_NAME_MEANING="meaning";//列：单词含义
+        public static final String COLUMN_NAME_SAMPLE="sample";//单词示例
+    }
+}
